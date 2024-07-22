704eb179a9f508f86b5dae1fd3273e5ecf882036
Akash_07 <barikakash396@gmail.com>
2024-07-22 13:05:39 +0530

    Deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth-routes.js";
 import messageRoutes from "./routes/message-routes.js";
@@ -8,15 +9,18 @@ import userRoutes from "./routes/user-routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
-
-const app = express();
+import {app, server} from "./socket/socket.js";
+// const app = express();
 
 const PORT = process.env.PORT || 5000;
 const MONGO_DB_URI = process.env.MONGO_DB_URI;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
+
 
 // app.get("/", (req, res) => {
 //   res.send("Hello World");
@@ -25,8 +29,13 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.listen(PORT, () => {
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
+
+server.listen(PORT, () => {
   connectToMongoDB(MONGO_DB_URI);
   console.log(`Server is running at ${PORT}`);
 });
--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,22 +1,40 @@
+import { useState } from "react";
 import { IoSearchSharp } from "react-icons/io5";
+import useConversation from "../../zustand/useConversation";
+import useGetConversations from "../../hooks/useGetConversations";
+import toast from "react-hot-toast";
 
 const SearchInput = () => {
+	const [search, setSearch] = useState("");
+	const { setSelectedConversation } = useConversation();
+	const { conversations } = useGetConversations();
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (!search) return;
+		if (search.length < 3) {
+			return toast.error("Search term must be at least 3 characters long");
+		}
+
+		const conversation = conversations.find((c) => c.fullName.toLowerCase().includes(search.toLowerCase()));
+
+		if (conversation) {
+			setSelectedConversation(conversation);
+			setSearch("");
+		} else toast.error("No such user found!");
+	};
+
 	return (
-		<form className='flex items-center gap-2'>
-			<label className="input input-bordered flex items-center gap-2">
-                <input type="text" className="grow" placeholder="Search" />
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    fill="currentColor"
-                    className="h-4 w-4 opacity-70">
-                    <path
-                    fillRule="evenodd"
-                    d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                    clipRule="evenodd" />
-                </svg>
+		<form onSubmit={handleSubmit} className='flex items-center gap-2'>
+			<label className="input input-bordered flex">
+                <input type="text" className="grow" placeholder="Search"value={search} onChange={(e) => setSearch(e.target.value)} />
+
+                <button type='submit'>
+				    <IoSearchSharp className='w-6 h-6' />
+			    </button>
             </label>
 		</form>
 	);
+
 };
 export default SearchInput;

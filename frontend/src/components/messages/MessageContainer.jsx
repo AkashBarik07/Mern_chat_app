8728ea743461f9e4c74d6acd113641582b9cf85b
Akash_07 <barikakash396@gmail.com>
2024-07-24 12:27:50 +0530

    git new changing

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -14,15 +14,15 @@ const MessageContainer = () => {
 	}, [setSelectedConversation]);
 
 	return (
-		<div className='md:min-w-[450px] flex flex-col'>
+		<div className='md:min-w-[615px] flex flex-col'>
 			{!selectedConversation ? (
 				<NoChatSelected />
 			) : (
 				<>
 					{/* Header */}
-					<div className='bg-slate-500 px-4 py-2 mb-2'>
-						<span className='label-text'>To:</span>{" "}
-						<span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
+					<div className='bg-black px-4 py-5 mb-2'>
+						<span className='label-text text-white'>To:</span>{" "}
+						<span className='text-gray-900 font-bold text-white'>{selectedConversation.fullName}</span>
 					</div>
 					<Messages />
 					<MessageInput />

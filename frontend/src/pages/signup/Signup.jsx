a54c0e22321d4abc0e171815ca5edc9fefa550cf
Akash_07 <barikakash396@gmail.com>
2024-07-24 13:14:25 +0530

    ald

--- frontend/src/pages/signup/Signup.jsx
+++ frontend/src/pages/signup/Signup.jsx
@@ -25,7 +25,7 @@ const SignUp = () => {
 
 	return (
 		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
-			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+			<div className='w-full p-6 rounded-lg shadow-md bg-gray-900 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-1'>
 				<h1 className='text-3xl font-semibold text-center text-gray-300'>
 					Sign Up <span className='text-blue-500'> ChatApp</span>
 				</h1>
@@ -79,7 +79,7 @@ const SignUp = () => {
 
 					<GenderCheckbox onCheckboxChange = {handleCheckboxChange} selectedGender = {inputs.gender} />
 
-					<Link className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block  text-gray-400' to='/login'>
+					<Link className='text-sm hover:underline hover:text-blue-400 mt-2 inline-block  text-blue-400' to='/login'>
 						Already have an account?
 					</Link>
 

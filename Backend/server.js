ffa488958d0ec1e221c49a911035789113376a85
Akash_07 <barikakash396@gmail.com>
2024-07-27 20:07:19 +0530

    commited are changes

--- Backend/server.js
+++ Backend/server.js
@@ -28,11 +28,11 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-})
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB(MONGO_DB_URI);
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -11,8 +10,5 @@ export default defineConfig({
         target: "http://localhost:5000",
       },
     },
-  },
-  build: {
-    outDir: "../dist", // Ensure the build output goes to the 'dist' directory in the root
   },
 });
